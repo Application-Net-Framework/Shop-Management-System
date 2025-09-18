796714480bfab23cc890a76fa9b5d04edf15dfa8
Siam <Siam@WIN-KCIJ9V9PQSF>
2025-09-18 10:35:01 -0700

    manager dashboard

--- LogPage/Program.cs
+++ LogPage/Program.cs
@@ -16,17 +16,17 @@ namespace App
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
 
             // Create and show splash screen
             //SplashForm splashForm = new SplashForm();
             //splashForm.Show();
 
             // Create the main login form but don't show it yet
-            LogPage loginForm = new LogPage();
+            //LogPage loginForm = new LogPage();
             // Center the login form as well
-            loginForm.StartPosition = FormStartPosition.CenterScreen;
+            //loginForm.StartPosition = FormStartPosition.CenterScreen;
 
             // Process messages while splash screen is shown
             //while (splashForm.Visible)
@@ -40,7 +40,7 @@ namespace App
             // These will never be executed because Application.Run() is blocking
             // Until the form is closed
             // Application.Run(new Cashier_Home());
-            Application.Run(new Cashier());
+            //Application.Run(new Cashier());
             //Application.Run(new Registration_Admin());
             //Application.Run(new Login_Admin());
             Application.Run(new Manager_Home());

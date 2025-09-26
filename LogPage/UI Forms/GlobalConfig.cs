64ad5d83959d00bc0bec2ebf911a31bafed30eb7
DESKTOP-HRPRSI4\Youtech BD <sumaiyaakterroshni184@gmail.com>
2025-09-27 02:12:58 +0600

    new

--- LogPage/UI Forms/GlobalConfig.cs
+++ LogPage/UI Forms/GlobalConfig.cs
@@ -1,17 +1,17 @@
 ﻿using System.Data.SqlClient;
 
-namespace App.UI_Forms
+namespace App.Configuration { 
+
+
+public static class GlobalConfig
 {
-   
-    public static class GlobalConfig
-    {
-        //Arif
-        public static string ConnectionString = @"Data Source=DESKTOP-ESC3M7E\SQLEXPRESS;Initial Catalog=GSM;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
-        //Roshni
-        // public static string ConnectionString = @"Data Source=DESKTOP-HRPRSI4\\SQLEXPRESS;Initial Catalog=GSMSDb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
-        //Siam
-        // public static string ConnectionString =
-        // Hacin
-        // public static string ConnectionString =
-    }
+    //Arif
+    //public static string ConnectionString = @"Data Source=DESKTOP-ESC3M7E\SQLEXPRESS;Initial Catalog=GSM;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+    //Roshni
+    public static string ConnectionString = @"Data Source=DESKTOP-HRPRSI4\SQLEXPRESS;Initial Catalog=GSMSDb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+    //Siam
+    // public static string ConnectionString =
+    // Hacin
+    // public static string ConnectionString =
+}
 }
